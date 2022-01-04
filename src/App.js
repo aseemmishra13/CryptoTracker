e1be8379b11ef4846b2ab68912a1b9b09591da2b
aseemmishra13 <mishraaseem05@gmail.com>
2022-01-04 19:54:46 +0530

    dynamically update coin value

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ function App() {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    axios
+   const getCoinDetails = async()=>{ 
+    const res = await axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
       )
-      .then((res) => {
+     
         setCoins(res.data);
         console.log(res.data);
-      })
-      .catch((error) => console.error(error));
-  }, []);
+    }
+      getCoinDetails()
+  }, [coins]);
 
   const handleChange = (e) => {
     setSearch(e.target.value);
